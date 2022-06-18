0b4dec141a887a574b820bd44448e9cb4a8b44ac
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-18 23:34:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+    // iterate over the array of numbers
+    for (let i = 0; i < array.length; i++) {
+      // for the current number, identify a complementary number that adds to our target
+      // (for example: if our number is 2, and the target is 5, the complementary number is 3)
+      const complement = target - array[i];
+      // iterate over the remaining numbers in the array
+      for (let j = i + 1; j < array.length; j++) {
+        // check if any of the remaining numbers is the complement
+        // if so, return true
+        if (array[j] === complement) return true;
+      }
+    }
+    // if we reach the end of the array, return false
+    return false;
 }
 
 /* 
@@ -8,10 +22,18 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  iterate over the remaining numbers in the array
+    check if any of the remaining numbers is the complement
+      if so, return true
+if we reach the end of the array, return false
 */
 
 /*
   Add written explanation of your solution here
+  I need to make a hasTargetSum function that checks if two numbers in an array add up to some target number. For example, if the array is [1,2,3,4] and the target number is 6, I know that 2 and 4 add up to 6, so I should return true. If I have the same array and the target is 10, no two numbers in the array add up to 10, so I should return false.
 */
 
 // You can run `node index.js` to view these console logs
